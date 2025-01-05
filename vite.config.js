b130dfc914f3456b5e03ab0c9a6916484c98967a
Jeremy Watt <jermwatt@gmail.com>
2025-01-05 16:46:10 -0700

    vite config add

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+// Replace 'repository-name' with your GitHub repository name
+export default defineConfig({
+  base: "/js-todo-timers/",
+  build: {
+    outDir: "dist",
+  },
+});
